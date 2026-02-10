a2b4b5e6a71ce69cf45c3dcbd0cb608d344e8a52
growwithnidhan <mail2nidhan@gmail.com>
2026-02-10 15:01:32 +0530

    Center content in React UI

--- react-webapp/src/App.jsx
+++ react-webapp/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "60px" }}>
+    <div style={{ textAlign: "center", marginTop: "60px",justifyContent:"center"}}>
       <h1>DevOps CI/CD Pipeline with React</h1>
       <p>Built using GitHub Actions, Slack ChatOps & Netlify</p>
       <p>Auto deployment on every commit</p>
