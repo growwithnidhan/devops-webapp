212f1c096f852899b7b48c83c8b0e6715c705dd5
growwithnidhan <mail2nidhan@gmail.com>
2026-02-10 15:04:21 +0530

    UI changes - centered content

--- react-webapp/src/App.jsx
+++ react-webapp/src/App.jsx
@@ -1,6 +1,15 @@
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "60px",justifyContent:"center"}}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        textAlign: "center",
+      }}
+    >
       <h1>DevOps CI/CD Pipeline with React</h1>
       <p>Built using GitHub Actions, Slack ChatOps & Netlify</p>
       <p>Auto deployment on every commit</p>
