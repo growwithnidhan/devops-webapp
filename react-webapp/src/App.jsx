34a6799d5b281cebcc1d751f8eb99f4a3410e9e3
growwithnidhan <mail2nidhan@gmail.com>
2026-02-10 14:15:35 +0530

    React app build integrated into CI

---
+++ react-webapp/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "60px" }}>
+      <h1>DevOps CI/CD Pipeline with React</h1>
+      <p>Built using GitHub Actions, Slack ChatOps & Netlify</p>
+      <p>Auto deployment on every commit</p>
+    </div>
+  );
+}
+
+export default App;
